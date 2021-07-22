6a5d7d1a5937cb617eae804c007f85343fb1c3b4
Ибраими Албин <albin.ibraimi@students.finki.ukim.mk>
2021-07-22 18:34:39 +0200

    Registration

---
+++ E_Voting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Voting.Startup))]
+namespace E_Voting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
